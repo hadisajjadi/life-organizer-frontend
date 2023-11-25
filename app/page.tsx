e38629b3657324754349f7cf7686888b1b9beea6
hadisajjadi <hadisajjadi@protonmail.com>
2023-11-25 15:53:34 +0330

    added mood tracker component & seperated card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import Card from "./_components/Card";
+import CardTitle from "./_components/CardTitle";
+import MoodTracker from "./_components/MoodTracker";
 import TaskList from "./_components/TaskList";
 
 export default function Home() {
   return (
     <div>
       <div className="w-full h-[90vh] pt-6">
-        <div className="max-w-md h-full">
-          <TaskList />
+        <div className="grid grid-cols-12 grid-rows-6 gap-6 w-full h-full">
+          <div className="col-span-3 row-span-full">
+            <TaskList />
+          </div>
+          <div className="col-span-3 row-span-3">
+            <MoodTracker />
+          </div>
         </div>
 
       </div>
